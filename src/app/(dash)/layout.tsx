54e7c456ac9b5e07e6d7da0cc6ecfe2896ec03b2
Aditya Jadhav <aditya.jadhav@browza.in>
2025-10-29 13:28:30 +0530

    Updated layout.tsx

--- src/app/(dash)/layout.tsx
+++ src/app/(dash)/layout.tsx
@@ -3,7 +3,7 @@ import AppHeader from "@/components/AppHeader";
 export default function DashLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-dvh bg-neutral-50">
-      <AppHeader variant="light" />
+      <AppHeader />
       <main className="mx-auto max-w-7xl p-6 text-gray-900">
         {children}
       </main>
