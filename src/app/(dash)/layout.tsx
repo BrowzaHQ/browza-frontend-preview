b33503327a6c3b39950a0e8fce37cea2cd419d09
Aditya Jadhav <aditya.jadhav@browza.in>
2025-10-29 13:05:24 +0530

    Created layout.tsx

---
+++ src/app/(dash)/layout.tsx
@@ -0,0 +1,12 @@
+import AppHeader from "@/components/AppHeader";
+
+export default function DashLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="min-h-dvh bg-neutral-50">
+      <AppHeader variant="light" />
+      <main className="mx-auto max-w-7xl p-6 text-gray-900">
+        {children}
+      </main>
+    </div>
+  );
+}
